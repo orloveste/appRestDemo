1b7d4cb3e0e84e65c15a0eba31d37205a8a617e3
orloveste <orlovevm@yahoo.com>
2020-12-18 11:28:39 +0200

    Initial commit

---
+++ src/main/java/testRest/appRestDemo/AppRestDemoApplication.java
@@ -0,0 +1,13 @@
+package testRest.appRestDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppRestDemoApplication.class, args);
+	}
+
+}
